0dad26387a2c89bb97b66a3e3d3f05bed50ffbf6
Samson Ojugo <samsonojugo@gmail.com>
2021-08-27 19:24:08 +0100

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php ob_start(); 
-$conn =new mysqli('localhost', 'root', '' , 'ecommerce_db');
-        // $conn =new mysqli('localhost', 'servdqvt_serviceswise', 'dequiet1' , 'servdqvt_skilled_db');
+$conn =new mysqli('localhost', 'username', 'password' , 'db');
+        
         if ($conn->connect_error) {
             echo "<b>Error:</b> Connection failed - $conn->connect_error";
         }
@@ -14,38 +14,6 @@ $conn =new mysqli('localhost', 'root', '' , 'ecommerce_db');
     $date = date('Y-m-d H:i:s');
 
     
-    
-    require 'vendor/autoload.php';
-use \Mailjet\Resources;
-    function sendMails2($from_mail, $from_name, $receiver_mail, $txt, $subject){
-  
-  
-  $mj = new \Mailjet\Client('97a2ab273bbdb0f23c15ac1ed39cf5f7','11d45a703c85817a08167a93660e12c4',true,['version' => 'v3.1']);
-  $body = [
-    'Messages' => [
-      [
-        'From' => [
-          'Email' => "$from_mail",
-          'Name' => "$from_name"
-        ],
-        'To' => [
-          [
-            'Email' => "$receiver_mail"
-            
-          ]
-        ],
-        'Subject' => "$subject",
-        // 'TextPart' => "My first Mailjet email",
-        'HTMLPart' => "$txt",
-        'CustomID' => "AppGettingStartedTest"
-      ]
-    ]
-  ];
-  $response = $mj->post(Resources::$Email, ['body' => $body]);
-  $response->success() && var_dump($response->getData());
-
-}
-
 
     function encode_txt( $q ) {
         $cryptKey  = 'qJB0rGtIn5UB1xG03efyCp';
